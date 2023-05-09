408e313e04dae1b202cedcfdc756b35f3045391f
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-09 13:21:27 +0100

    create auth axios

--- src/lib/authAxios.js
+++ src/lib/authAxios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const authAxios = axios.create({
   baseURL: "http://localhost:5007",
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 });
