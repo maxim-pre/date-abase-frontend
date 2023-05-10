282a637d742ca76816d145ea2f4e8f35efae2f5c
Louise Jones <louisejones94@live.co.uk>
2023-05-10 12:15:23 +0100

    Add matching functionality

--- src/components/userCard.js
+++ src/components/userCard.js
@@ -1,5 +1,20 @@
 import profile from '../../src/static/images/avatar.png';
-const UserCard = ({firstName, lastName, interestedInGender, isMatched, createdAt}) => {
+import { addMatch, removeMatch } from '../lib/matchesApi';
+
+const UserCard = ({currentUser, id, firstName, lastName, interestedInGender, isMatched, createdAt}) => {
+
+    function handleAddMatch() {
+        addMatch(currentUser.id, id);
+        addMatch(id, currentUser.id);
+        // some code to change the style of the button(s)
+    }
+
+    function handleRemoveMatch() {
+        removeMatch(currentUser.id, id);
+        removeMatch(id, currentUser.id);
+        // some code to change the style of the button(s)
+    }
+
     return (
         <div className="border w-full">
             <h2 className="text-xl">Name</h2>
@@ -7,8 +22,8 @@ const UserCard = ({firstName, lastName, interestedInGender, isMatched, createdAt
             
             <img className="object-cover w-full rounded-full" src={profile} />
             <div>
-                <button className="border-2 border-black rounded-full px-2 ">No</button>
-                <button className="border-2 border-black rounded-full px-2 "> Yes</button>
+                <button className="border-2 border-black rounded-full px-2 " onClick={handleRemoveMatch}>No</button>
+                <button className="border-2 border-black rounded-full px-2 " onClick={handleAddMatch}> Yes</button>
             </div>
         </div>
     )
