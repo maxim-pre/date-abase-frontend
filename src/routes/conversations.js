72faf0f42400c6d05732adceafc1658bb9d5adfc
Louise Jones <louisejones94@live.co.uk>
2023-05-10 09:54:12 +0100

    Enable server to run

--- src/routes/conversations.js
+++ src/routes/conversations.js
@@ -1,19 +1,19 @@
 import Conversations from "../components/conversations/conversations";
 import SelectedConversation from "../components/conversations/selectedConversation";
 
-const ConversationsPage = () =>{
+const ConversationsPage = ({user}) =>{
     return (
     <div>
         <h1>Conversations</h1>
         <p>This is the conversations page</p>
         <div>
             {/* Get all the conversations for this user */}
-            <Conversations />
+            <Conversations user={user}/>
             {/* Display the conversations in a list */}
         </div>
         <div>
             {/* When they click on a conversation, it shows all the messages on the right hand side */}
-            <SelectedConversation />
+            {/* <SelectedConversation user={user}/> */}
             {/* On an individual conversation, show a form where the user can type and submit a reply */}
         </div>
         
