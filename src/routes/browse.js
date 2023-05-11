2f15ffa7ea90631799aad6045ea17f5660fb84c0
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-11 16:29:11 +0100

    add _redirects to public folder to tell netlifly to ensure browser router works

--- src/routes/browse.js
+++ src/routes/browse.js
@@ -1,60 +1,68 @@
-import React, {useEffect, useState} from'react';
-import { getAllUsers } from '../lib/usersApi';
-import UserCard from '../components/userCard';
+import React, { useEffect, useState } from "react";
+import { getAllUsers } from "../lib/usersApi";
+import UserCard from "../components/userCard";
 
-const BrowsePage = ({user}) =>{
-    // Feed through user information to the UserCard component
+const BrowsePage = ({ user }) => {
+  // Feed through user information to the UserCard component
 
-    // current user information
-    const currentUser = user;
+  // current user information
+  const currentUser = user;
 
-    // all user information
-    const [allUsers, setAllUsers] = useState([]);
+  // all user information
+  const [allUsers, setAllUsers] = useState([]);
 
-    useEffect(() => {
-        getAllUsers()
-            .then(data => {
-                return data.json()
-            })
-            .then(results => {
-                let otherUsers = results.users.filter(person => person._id !== currentUser._id)
-                setAllUsers(otherUsers)
-            })
-    }, []);
+  useEffect(() => {
+    getAllUsers()
+      .then((data) => {
+        return data.json();
+      })
+      .then((results) => {
+        let otherUsers = results.users.filter(
+          (person) => person._id !== currentUser._id
+        );
+        setAllUsers(otherUsers);
+      });
+  }, []);
 
-    
-    
-    const displayUsers = allUsers.map(otherUser => {
+  console.log(allUsers);
+  const displayUsers = allUsers.map((otherUser) => {
+    // Check if current user is in this user's matches array
+    if (otherUser.matches.includes(currentUser._id)) {
+      otherUser.isMatched = true;
+    } else {
+      otherUser.isMatched = false;
+    }
 
-        // Check if current user is in this user's matches array
-        if (otherUser.matches.includes(currentUser._id)) {
-            otherUser.isMatched=true;
-        } else {
-            otherUser.isMatched=false;
-        }
-
-        return (<UserCard 
-            currentUser={currentUser}
-            otherUser={otherUser}
-            key={otherUser._id} 
-            id={otherUser._id}
-            firstName={otherUser.firstName}
-            lastName={otherUser.lastName}
-            interestedInGender={otherUser.interestedInGender}
-            createdAt={otherUser.createdAt}
-            isMatched={otherUser.isMatched}
-        />)
-    })
-    
     return (
-        <div className="bg-[url('../src/static/images/AdobeStock_88856691.jpeg')] bg-top bg-cover pb-16">
-            <h1 className="text-5xl text-center  text-white text-shadow py-16">Find your perfect (pair programming) partner!</h1>
-            <p className='text-center text-2xl mb-8 text-gray-100 special-text-shadow'>Click that Y button and let the sparks fly - with just one tap, you could be on your way to a steamy romance that'll have your heart racing and your phone buzzing!</p>
-            <div className='grid lg:grid-cols-3 place-items-center md:grid-cols-2'>
-                {displayUsers}
-            </div>
-        </div>
-    )
-}
+      <UserCard
+        currentUser={currentUser}
+        otherUser={otherUser}
+        key={otherUser._id}
+        id={otherUser._id}
+        firstName={otherUser.firstName}
+        lastName={otherUser.lastName}
+        interestedInGender={otherUser.interestedInGender}
+        createdAt={otherUser.createdAt}
+        isMatched={otherUser.isMatched}
+      />
+    );
+  });
+
+  return (
+    <div className="bg-[url('../src/static/images/AdobeStock_88856691.jpeg')] bg-top bg-cover pb-16">
+      <h1 className="text-5xl text-center  text-white text-shadow py-16">
+        Find your perfect (pair programming) partner!
+      </h1>
+      <p className="text-center text-2xl mb-8 text-gray-100 special-text-shadow">
+        Click that Y button and let the sparks fly - with just one tap, you
+        could be on your way to a steamy romance that'll have your heart racing
+        and your phone buzzing!
+      </p>
+      <div className="grid lg:grid-cols-3 place-items-center md:grid-cols-2">
+        {displayUsers}
+      </div>
+    </div>
+  );
+};
 
 export default BrowsePage;
